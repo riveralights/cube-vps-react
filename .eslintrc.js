e1493ac2e79d582ce26b1d8e4b4fcc06d4aebc15
riveralights <riveralights@gmail.com>
2021-06-14 20:38:26 +0700

    Buttons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    settings: {
-        "import/resolver": {
-            node: {
-                paths: ['src']
-            }
-        }
-    }
-}
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+      },
+    },
+  },
+};
---
+++ src/scenes/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Button isPrimary isWideMobile isSmall>
+        Sign Up
+      </Button>
+    </div>
+  );
+}
