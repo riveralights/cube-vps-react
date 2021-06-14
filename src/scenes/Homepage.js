ddeab41c47be9be3b2c212e9409da80b7ccd3f68
riveralights <riveralights@gmail.com>
2021-06-14 21:52:56 +0700

    Client Section

--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import Section from "elements/Section";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import Client from "parts/Client";
 
 export default function Homepage() {
   return (
@@ -11,6 +13,11 @@ export default function Homepage() {
         <Section className="hero illustration-section-01" isCenteredContent>
           <Hero></Hero>
         </Section>
+        <Fade bottom delay={600}>
+          <Section className="clients">
+            <Client></Client>
+          </Section>
+        </Fade>
       </main>
     </div>
   );
